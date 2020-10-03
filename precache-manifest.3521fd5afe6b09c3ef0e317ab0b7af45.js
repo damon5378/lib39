1a1d4b96eb72bbf000dc25837389dcbef02260f4
damon5378 <iosifov.konstantin@gmail.com>
2020-10-03 09:13:16 +0300

    Updates

---
+++ precache-manifest.3521fd5afe6b09c3ef0e317ab0b7af45.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0f8332f7766f3fe47ee0e8e9bbf8112",
+    "url": "/lib39/index.html"
+  },
+  {
+    "revision": "456f73ca255c60305dd6",
+    "url": "/lib39/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "28652cd51d955ce23306",
+    "url": "/lib39/static/js/2.f60fc2d4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/lib39/static/js/2.f60fc2d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "456f73ca255c60305dd6",
+    "url": "/lib39/static/js/main.d271333f.chunk.js"
+  },
+  {
+    "revision": "b845863c6fb499f789bd",
+    "url": "/lib39/static/js/runtime-main.1402ae4a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/lib39/static/media/logo.5d5d9eef.svg"
+  }
+]);
